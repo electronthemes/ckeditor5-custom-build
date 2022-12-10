f45dab69ff914f223234ab790cfe996ed3c2b23c
Shoaib Sharif <shoaib19j@gmail.com>
2022-12-10 12:43:36 -0600

    build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,37 +2,39 @@
  * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
-import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage.js";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock.js";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
 import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed.js";
+import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage.js";
 import Image from "@ckeditor/ckeditor5-image/src/image.js";
 import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert.js";
 import ImageTextAlternative from "@ckeditor/ckeditor5-image/src/imagetextalternative";
 import imageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert.js";
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
 import Link from "@ckeditor/ckeditor5-link/src/link.js";
 import List from "@ckeditor/ckeditor5-list/src/list.js";
-import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle.js";
+import ListProperties from "@ckeditor/ckeditor5-list/src/listproperties";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
-import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js";
-import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
 import Table from "@ckeditor/ckeditor5-table/src/table.js";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
-import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js";
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
+  CodeBlock,
   Autoformat,
   AutoImage,
   BlockQuote,
@@ -50,7 +52,7 @@ Editor.builtinPlugins = [
   Italic,
   Link,
   List,
-  ListStyle,
+  ListProperties,
   Paragraph,
   SimpleUploadAdapter,
   Strikethrough,
@@ -70,6 +72,7 @@ Editor.defaultConfig = {
       "link",
       "bulletedList",
       "numberedList",
+      "codeBlock",
       "strikethrough",
       "underline",
       "|",
@@ -92,6 +95,23 @@ Editor.defaultConfig = {
   },
   table: {
     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
+  codeBlock: {
+    languages: [
+      { language: "plaintext", label: "Plain text" }, // The default language.
+      { language: "c", label: "C" },
+      { language: "cs", label: "C#" },
+      { language: "cpp", label: "C++" },
+      { language: "css", label: "CSS" },
+      { language: "diff", label: "Diff" },
+      { language: "html", label: "HTML" },
+      { language: "java", label: "Java" },
+      { language: "javascript", label: "JavaScript" },
+      { language: "php", label: "PHP" },
+      { language: "python", label: "Python" },
+      { language: "ruby", label: "Ruby" },
+      { language: "typescript", label: "TypeScript" },
+    ],
   },
 };
 export default Editor;
